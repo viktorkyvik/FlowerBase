ca354fc29e42014de9b793e45bafb43f577aadd3
viktorkyvik <viktor.kyvik@gmail.com>
2026-08-03 18:55:40 +0200

    finished up prototype, next up is sign-in system

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let wt_lvl;
 let ti;
 let wa;
+let mw;
 let sm;
 
 wt_lvl = 0
 ti = 24
 wa = 500
+mw = 0
 sm = 0
 
 document.getElementById("wt_lvl").textContent = wt_lvl;
@@ -21,24 +23,31 @@ const mw_info = document.getElementById("mw_info")
 const sm_info = document.getElementById("sm_info")
 const sm_plot_info = document.getElementById("sm_plot_info")
 
-const ti_adj = document.getElementById("ti_adj")
+const ti_btn = document.getElementById("ti_btn")
 const ti_input = document.getElementById("ti_input")
 const ti_save = document.getElementById("ti_save")
 const ti_cancel = document.getElementById("ti_cancel")
 
-const wa_adj = document.getElementById("wa_adj")
+const wa_btn = document.getElementById("wa_btn")
 const wa_input = document.getElementById("wa_input")
 const wa_save = document.getElementById("wa_save")
 const wa_cancel = document.getElementById("wa_cancel")
 
-const mw_amount = document.getElementById("mw_amount")
-
+const mw_btn = document.getElementById("mw_btn")
+const mw_input = document.getElementById("mw_input")
+const mw_conf = document.getElementById("mw_conf")
+const mw_canc = document.getElementById("mw_canc")
+const mw_yes = document.getElementById("mw_yes")
+const mw_no = document.getElementById("mw_no")
 
 const wt_info_popup = document.getElementById("wt_info_popup");
 const ti_info_popup = document.getElementById("ti_info_popup");
-const ti_adj_popup = document.getElementById("ti_adj_popup");
+const ti_btn_popup = document.getElementById("ti_btn_popup");
 const wa_info_popup = document.getElementById("wa_info_popup");
-const wa_adj_popup = document.getElementById("wa_adj_popup");
+const wa_btn_popup = document.getElementById("wa_btn_popup");
+const mw_info_popup = document.getElementById("mw_info_popup");
+const mw_btn_popup = document.getElementById("mw_btn_popup");
+const mw_verify_popup = document.getElementById("mw_verify_popup")
 const sm_info_popup = document.getElementById("sm_info_popup");
 const sm_plot_info_popup = document.getElementById("sm_plot_info_popup");
 
@@ -52,6 +61,9 @@ ti_info.addEventListener("click", () => {
 wa_info.addEventListener("click", () => {
     wa_info_popup.showModal()
 })
+mw_info.addEventListener("click", () => {
+    mw_info_popup.showModal()
+})
 sm_info.addEventListener("click", () => {
     sm_info_popup.showModal()
 })
@@ -60,9 +72,9 @@ sm_plot_info.addEventListener("click", () => {
 })
 
 
-ti_adj.addEventListener("click", () => {
+ti_btn.addEventListener("click", () => {
     ti_input.value = ti;
-    ti_adj_popup.showModal();
+    ti_btn_popup.showModal();
 })
 ti_save.addEventListener("click", () => {
     const tiNew = parseInt(ti_input.value, 10);
@@ -70,30 +82,58 @@ ti_save.addEventListener("click", () => {
     if (!isNaN(tiNew)) {
         ti = tiNew;
         document.getElementById("ti").textContent = ti;
-        ti_adj_popup.close();
+        ti_btn_popup.close();
     } else {
         alert("Please enter a valid whole number.");
     }
 })
 ti_cancel.addEventListener("click", () => {
-    ti_adj_popup.close();
+    ti_btn_popup.close();
 })
 
-wa_adj.addEventListener("click", () => {
+
+wa_btn.addEventListener("click", () => {
     wa_input.value = wa;
-    wa_adj_popup.showModal();
+    wa_btn_popup.showModal();
 })
 wa_save.addEventListener("click", () => {
     const waNew = parseInt(wa_input.value, 10);
 
-    if (!isNaN(waNew)) {
+    if (!isNaN(waNew) && waNew >= 100) {    
         wa = waNew;
         document.getElementById("wa").textContent = wa;
-        wa_adj_popup.close();
-    } else {
+        wa_btn_popup.close();
+    } else if (waNew < 100) {
+        alert("Please enter at least 100ml.")
+    } 
+    else {
         alert("Please enter a valid whole number.");
     }
 })
 wa_cancel.addEventListener("click", () => {
-    wa_adj_popup.close();
+    wa_btn_popup.close();
+})
+
+
+mw_btn.addEventListener("click", () => {
+    mw_input.value = 0;
+    mw_btn_popup.showModal();
+})
+mw_conf.addEventListener("click", () => {
+    const mwNew = parseInt(mw_input.value, 10);
+
+    if (!isNaN(mwNew) && mwNew >= 100) {
+        mw_btn_popup.close();
+        mw_verify_popup.showModal();
+        mw_yes.addEventListener("click", () => {
+            mw = mwNew;
+            mw_verify_popup.close();
+        })
+        mw_no.addEventListener("click", () => {
+            mw_verify_popup.close();
+        })
+    }
+})
+mw_canc.addEventListener("click", () => {
+    mw_btn_popup.close();
 })
