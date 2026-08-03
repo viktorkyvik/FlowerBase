aa32580cc84b2474673f618db177bbcf5d637b85
viktorkyvik <viktor.kyvik@gmail.com>
2026-08-03 20:52:11 +0200

    added soil moisture threshold as timer option. fixed min and max options. added fake temp numbers to water tank and soil moisture

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 let wt_lvl;
 let ti;
+let smt;
 let wa;
 let mw;
 let sm;
+let type_active;
 
-wt_lvl = 0
+wt_lvl = 25.3
 ti = 24
+smt = 25
 wa = 500
 mw = 0
-sm = 0
+sm = 41
+type_active = 0
 
 document.getElementById("wt_lvl").textContent = wt_lvl;
 document.getElementById("ti").textContent = ti;
+document.getElementById("smt").textContent = smt;
 document.getElementById("wa").textContent = wa;
 document.getElementById("sm").textContent = sm;
 
 
+
 const wt_info = document.getElementById("wt_info")
 const ti_info = document.getElementById("ti_info")
+const smt_info = document.getElementById("smt_info")
 const wa_info = document.getElementById("wa_info")
 const mw_info = document.getElementById("mw_info")
 const sm_info = document.getElementById("sm_info")
@@ -27,6 +34,15 @@ const ti_btn = document.getElementById("ti_btn")
 const ti_input = document.getElementById("ti_input")
 const ti_save = document.getElementById("ti_save")
 const ti_cancel = document.getElementById("ti_cancel")
+const changeto_smt = document.getElementById("changeto_smt")
+const type_ti = document.getElementById("type_ti")
+
+const smt_btn = document.getElementById("smt_btn")
+const smt_input = document.getElementById("smt_input")
+const smt_save = document.getElementById("smt_save")
+const smt_cancel = document.getElementById("smt_cancel")
+const changeto_ti = document.getElementById("changeto_ti")
+const type_smt = document.getElementById("type_smt")
 
 const wa_btn = document.getElementById("wa_btn")
 const wa_input = document.getElementById("wa_input")
@@ -43,6 +59,8 @@ const mw_no = document.getElementById("mw_no")
 const wt_info_popup = document.getElementById("wt_info_popup");
 const ti_info_popup = document.getElementById("ti_info_popup");
 const ti_btn_popup = document.getElementById("ti_btn_popup");
+const smt_info_popup = document.getElementById("smt_info_popup");
+const smt_btn_popup = document.getElementById("smt_btn_popup");
 const wa_info_popup = document.getElementById("wa_info_popup");
 const wa_btn_popup = document.getElementById("wa_btn_popup");
 const mw_info_popup = document.getElementById("mw_info_popup");
@@ -57,6 +75,9 @@ wt_info.addEventListener("click", () => {
 })
 ti_info.addEventListener("click", () => {
     ti_info_popup.showModal()
+})
+smt_info.addEventListener("click", () => {
+    smt_info_popup.showModal()
 })
 wa_info.addEventListener("click", () => {
     wa_info_popup.showModal()
@@ -79,17 +100,47 @@ ti_btn.addEventListener("click", () => {
 ti_save.addEventListener("click", () => {
     const tiNew = parseInt(ti_input.value, 10);
 
-    if (!isNaN(tiNew)) {
+    if (!isNaN(tiNew) && tiNew > 0) {
         ti = tiNew;
         document.getElementById("ti").textContent = ti;
         ti_btn_popup.close();
     } else {
-        alert("Please enter a valid whole number.");
+        alert("Please enter a valid whole number above 0.");
     }
 })
 ti_cancel.addEventListener("click", () => {
     ti_btn_popup.close();
 })
+changeto_smt.addEventListener("click", () => {
+    type_ti.style.display = "none"
+    type_smt.style.display = "block"
+    type_active = 1
+})
+
+smt_btn.addEventListener("click", () => {
+    smt_input.value = smt;
+    smt_btn_popup.showModal();
+})
+smt_save.addEventListener("click", () => {
+    const smtNew = parseInt(smt_input.value, 10);
+
+    if (!isNaN(smtNew) && smtNew >= 5 && smtNew <= 100) {
+        smt = smtNew;
+        document.getElementById("smt").textContent = smt;
+        smt_btn_popup.close();
+    } else {
+        alert("Please enter a valid whole number between 5 and 100.");
+    }
+})
+smt_cancel.addEventListener("click", () => {
+    smt_btn_popup.close();
+})
+changeto_ti.addEventListener("click", () => {
+    type_smt.style.display = "none"
+    type_ti.style.display = "block"
+    type_active = 0
+})
+
 
 
 wa_btn.addEventListener("click", () => {
@@ -99,15 +150,12 @@ wa_btn.addEventListener("click", () => {
 wa_save.addEventListener("click", () => {
     const waNew = parseInt(wa_input.value, 10);
 
-    if (!isNaN(waNew) && waNew >= 100) {    
+    if (!isNaN(waNew) && waNew >= 100 && waNew <= 2000) {    
         wa = waNew;
         document.getElementById("wa").textContent = wa;
         wa_btn_popup.close();
-    } else if (waNew < 100) {
-        alert("Please enter at least 100ml.")
-    } 
-    else {
-        alert("Please enter a valid whole number.");
+    } else {
+        alert("Please enter a valid whole number between 100 and 2000.");
     }
 })
 wa_cancel.addEventListener("click", () => {
@@ -122,7 +170,7 @@ mw_btn.addEventListener("click", () => {
 mw_conf.addEventListener("click", () => {
     const mwNew = parseInt(mw_input.value, 10);
 
-    if (!isNaN(mwNew) && mwNew >= 100) {
+    if (!isNaN(mwNew) && mwNew >= 100 && mwNew <= 2000) {
         mw_btn_popup.close();
         mw_verify_popup.showModal();
         mw_yes.addEventListener("click", () => {
@@ -132,6 +180,8 @@ mw_conf.addEventListener("click", () => {
         mw_no.addEventListener("click", () => {
             mw_verify_popup.close();
         })
+    } else {
+        alert("Please enter a valid whole number between 100 and 2000.");
     }
 })
 mw_canc.addEventListener("click", () => {
