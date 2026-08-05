1b0048b66e3f21bb16710d11afa3874c122c67b6
viktorkyvik <viktor.kyvik@gmail.com>
2026-08-05 20:21:21 +0200

    added database
    Added a database with saved settings for 1 user. Also added fake sensor data to send and test. Everything works. Next i should make the graph for this weeks sm levels

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-let wt_lvl;
-let ti;
-let smt;
-let wa;
-let mw;
-let sm;
-let type_active;
+//settings
+let ti = 24;
+let smt = 25;
+let wa = 500;
+let mw = 0;
+let type_active = "moisture";
+//data
+let wt_lvl = 0;
+let sm = 0;
 
-wt_lvl = 25.3
-ti = 24
-smt = 25
-wa = 500
-mw = 0
-sm = 41
-type_active = 0
-
-document.getElementById("wt_lvl").textContent = wt_lvl;
-document.getElementById("ti").textContent = ti;
-document.getElementById("smt").textContent = smt;
-document.getElementById("wa").textContent = wa;
-document.getElementById("sm").textContent = sm;
-
-
-
+//declares all consts from html
 const wt_info = document.getElementById("wt_info")
 const ti_info = document.getElementById("ti_info")
 const smt_info = document.getElementById("smt_info")
@@ -69,7 +56,122 @@ const mw_verify_popup = document.getElementById("mw_verify_popup")
 const sm_info_popup = document.getElementById("sm_info_popup");
 const sm_plot_info_popup = document.getElementById("sm_plot_info_popup");
 
+//functions
+async function loadSettings() {
+    const response = await fetch("/api/settings");
 
+    if (!response.ok) {
+        console.error("Could not load settings");
+        return;
+    }
+
+    const settings = await response.json();
+
+    console.log("Settings received:", settings);
+
+    ti = settings.watering_interval;
+    smt = settings.moisture_threshold;
+    wa = settings.watering_amount;
+    type_active = settings.watering_mode;
+
+    document.getElementById("ti").textContent = ti;
+    document.getElementById("smt").textContent = smt;
+    document.getElementById("wa").textContent = wa;
+
+    if (type_active === "interval") {
+        type_ti.style.display = "block";
+        type_smt.style.display = "none";
+    } else {
+        type_ti.style.display = "none";
+        type_smt.style.display = "block";
+    }
+}
+
+async function saveSettings() {
+    const response = await fetch("/api/settings", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            watering_interval: ti,
+            moisture_threshold: smt,
+            watering_amount: wa,
+            watering_mode: type_active
+        })
+    });
+
+    if (!response.ok) {
+        alert("Could not save settings");
+        return;
+    }
+
+    console.log("Settings saved");
+}
+
+async function sendFakeSensorReading() {
+    const fakeSoilMoisture = Math.floor(Math.random() * 60) + 20;
+    const fakeTankLevel = 25.3;
+
+    const response = await fetch("/api/readings", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            soil_moisture: fakeSoilMoisture,
+            tank_level: fakeTankLevel
+        })
+    });
+
+    if (response.ok) {
+        console.log("Fake sensor data sent");
+    }
+}
+
+async function loadLatestReading() {
+    const response = await fetch("/api/readings/latest");
+
+    if (!response.ok) {
+        console.error("Could not load sensor data");
+        return;
+    }
+
+    const reading = await response.json();
+
+    if (!reading) {
+        return;
+    }
+
+    sm = reading.soil_moisture;
+    wt_lvl = reading.tank_level
+
+    document.getElementById("sm").textContent = sm;
+    document.getElementById("wt_lvl").textContent = wt_lvl;
+}
+
+async function recordWatering(amount) {
+    const response = await fetch("/api/watering", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            amount_ml: amount,
+            method: "manual"
+        })
+    });
+
+    if (!response.ok) {
+        alert ("Could not record watering");
+        return;
+    }
+
+    return true;
+}
+
+//event listeners
+//info buttons
 wt_info.addEventListener("click", () => {
     wt_info_popup.showModal()
 })
@@ -92,20 +194,24 @@ sm_plot_info.addEventListener("click", () => {
     sm_plot_info_popup.showModal()
 })
 
-
+//time interval buttons
 ti_btn.addEventListener("click", () => {
     ti_input.value = ti;
     ti_btn_popup.showModal();
 })
-ti_save.addEventListener("click", () => {
+ti_save.addEventListener("click", async () => {
     const tiNew = parseInt(ti_input.value, 10);
 
     if (!isNaN(tiNew) && tiNew > 0) {
         ti = tiNew;
+
         document.getElementById("ti").textContent = ti;
+
         ti_btn_popup.close();
+
+        await saveSettings();
     } else {
-        alert("Please enter a valid whole number above 0.");
+        alert("Please enter a valid whole number above 0.");    
     }
 })
 ti_cancel.addEventListener("click", () => {
@@ -114,20 +220,25 @@ ti_cancel.addEventListener("click", () => {
 changeto_smt.addEventListener("click", () => {
     type_ti.style.display = "none"
     type_smt.style.display = "block"
-    type_active = 1
+    type_active = "moisture"
 })
 
+//soil moisture threshold buttons
 smt_btn.addEventListener("click", () => {
     smt_input.value = smt;
     smt_btn_popup.showModal();
 })
-smt_save.addEventListener("click", () => {
+smt_save.addEventListener("click", async () => {
     const smtNew = parseInt(smt_input.value, 10);
 
     if (!isNaN(smtNew) && smtNew >= 5 && smtNew <= 100) {
         smt = smtNew;
+
         document.getElementById("smt").textContent = smt;
+
         smt_btn_popup.close();
+
+        await saveSettings();
     } else {
         alert("Please enter a valid whole number between 5 and 100.");
     }
@@ -138,22 +249,25 @@ smt_cancel.addEventListener("click", () => {
 changeto_ti.addEventListener("click", () => {
     type_smt.style.display = "none"
     type_ti.style.display = "block"
-    type_active = 0
+    type_active = "interval"
 })
 
-
-
+//watering amount buttons
 wa_btn.addEventListener("click", () => {
     wa_input.value = wa;
     wa_btn_popup.showModal();
 })
-wa_save.addEventListener("click", () => {
+wa_save.addEventListener("click", async() => {
     const waNew = parseInt(wa_input.value, 10);
 
     if (!isNaN(waNew) && waNew >= 100 && waNew <= 2000) {    
         wa = waNew;
+
         document.getElementById("wa").textContent = wa;
+
         wa_btn_popup.close();
+
+        await saveSettings();
     } else {
         alert("Please enter a valid whole number between 100 and 2000.");
     }
@@ -162,7 +276,7 @@ wa_cancel.addEventListener("click", () => {
     wa_btn_popup.close();
 })
 
-
+//manual watering buttons
 mw_btn.addEventListener("click", () => {
     mw_input.value = 0;
     mw_btn_popup.showModal();
@@ -173,8 +287,9 @@ mw_conf.addEventListener("click", () => {
     if (!isNaN(mwNew) && mwNew >= 100 && mwNew <= 2000) {
         mw_btn_popup.close();
         mw_verify_popup.showModal();
-        mw_yes.addEventListener("click", () => {
+        mw_yes.addEventListener("click", async () => {
             mw = mwNew;
+            await recordWatering(mwNew);
             mw_verify_popup.close();
         })
         mw_no.addEventListener("click", () => {
@@ -187,3 +302,8 @@ mw_conf.addEventListener("click", () => {
 mw_canc.addEventListener("click", () => {
     mw_btn_popup.close();
 })
+
+loadSettings();
+setInterval(sendFakeSensorReading, 5000);
+loadLatestReading();
+setInterval(loadLatestReading, 5000);
---
+++ server.js
@@ -0,0 +1,280 @@
+const express = require("express");
+const Database = require("better-sqlite3");
+
+const app = express();
+const PORT = 3000;
+
+
+const db = new Database("flowerbase.db")
+
+app.use(express.json());
+app.use(express.static(__dirname));
+
+//db setup
+db.exec(`
+    CREATE TABLE IF NOT EXISTS settings (
+        id INTEGER PRIMARY KEY CHECK (id = 1),
+        watering_interval INTEGER NOT NULL DEFAULT 24,
+        moisture_threshold INTEGER NOT NULL DEFAULT 25,
+        watering_amount INTEGER NOT NULL DEFAULT 500,
+        watering_mode TEXT NOT NULL DEFAULT 'moisture'
+    );
+
+    CREATE TABLE IF NOT EXISTS sensor_readings (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        soil_moisture REAL NOT NULL,
+        tank_level REAL NOT NULL,
+        recorded_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+
+    CREATE TABLE IF NOT EXISTS watering_events (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        amount_ml INTEGER NOT NULL,
+        method TEXT NOT NULL,
+        watered_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+    
+    CREATE TABLE IF NOT EXISTS device (
+        id INTEGER PRIMARY KEY CHECK (id = 1),
+        device_name TEXT,
+        device_id TEXT,
+        last_seen DATETIME
+    );
+`);
+
+//default settings row if doesnt exist
+db.prepare(`
+    INSERT OR IGNORE INTO settings
+    (id, watering_interval, moisture_threshold, watering_amount, watering_mode)
+    VALUES (1, 24, 25, 500, 'moisture')
+`).run();
+
+//default device row if doesnt exist
+db.prepare(`
+    INSERT OR IGNORE INTO device
+    (id, device_name, device_id)
+    VALUES (1, 'Flowerbase ESP32', NULL)
+`).run();
+
+
+
+//gets settings
+app.get("/api/settings", (req, res) => {
+    const settings = db.prepare(`
+        SELECT *
+        FROM settings
+        WHERE id = 1    
+    `).get();
+
+    res.json(settings);
+});
+
+//saves settings
+app.put("/api/settings", (req, res) => {
+    const {
+        watering_interval,
+        moisture_threshold,
+        watering_amount,
+        watering_mode,
+    } = req.body;
+
+    if (
+        !Number.isInteger(watering_interval) ||
+        watering_interval < 1
+    ) {
+        return res.status(400).json({
+            error: "Invalid watering interval"
+        })
+    }
+
+    if (
+        !Number.isInteger(moisture_threshold) ||
+        moisture_threshold < 5 ||
+        moisture_threshold > 100
+    ) return res.status(400).json({
+        error: "Invalid moisture threshold"
+    });
+
+    if (
+        !Number.isInteger(watering_amount) ||
+        watering_amount < 100 ||
+        watering_amount > 2000
+    ) return res.status(400).json({
+        error: "Invalid watering amount"
+    })
+
+    if (
+        watering_mode !== "interval" &&
+        watering_mode !== "moisture"
+    ) {
+        return res.status(400).json ({
+            error: "Invalid watering mode"
+        });
+    }
+
+    db.prepare(`
+        UPDATE settings
+        SET
+            watering_interval = ?,
+            moisture_threshold = ?,
+            watering_amount = ?,
+            watering_mode = ?
+        WHERE id = 1
+    `).run(
+        watering_interval,
+        moisture_threshold,
+        watering_amount,
+        watering_mode
+    );
+
+    const settings = db.prepare(`
+        SELECT *
+        FROM settings
+        WHERE id = 1    
+    `).get();
+
+    res.json(settings);
+});
+
+
+//recieve sensor data
+app.post("/api/readings", (req, res) => {
+    const {
+        soil_moisture,
+        tank_level
+    } = req.body;
+
+    if (
+        typeof soil_moisture !== "number" ||
+        typeof tank_level !== "number"
+    ) {
+        return res.status(400).json({
+            error: "Invalid sensor data"
+        });
+    }
+
+    const result = db.prepare(`
+        INSERT INTO sensor_readings
+        (soil_moisture, tank_level)
+        VALUES (?, ?)
+    `).run(
+        soil_moisture,
+        tank_level
+    )
+
+    res.json({
+        success: true,
+        id: result.lastInsertRowid
+    });
+});
+
+//gets all sensor data
+app.get("/api/readings", (req, res) => {
+    const readings = db.prepare(`
+        SELECT *
+        FROM sensor_readings
+        ORDER BY recorded_at ASC
+    `).all();
+
+    res.json(readings);
+});
+//get latest sensor data
+app.get("/api/readings/latest", (req, res) => {
+    const reading = db.prepare(`
+        SELECT *
+        FROM sensor_readings
+        ORDER BY recorded_at DESC
+        LIMIT 1
+    `).get();
+
+    res.json(reading);
+})
+
+
+//record watering
+app.post("/api/watering", (req, res) => {
+    const {
+        amount_ml,
+        method
+    } = req.body;
+
+    if (
+        !Number.isInteger(amount_ml) ||
+        amount_ml < 100 ||
+        amount_ml > 2000
+    ) {
+        return res.status(400).json({
+            error: "Invalid watering amount"
+        });
+    }
+
+    if (
+        method !== "manual" &&
+        method !== "automatic"
+    ) {
+        return res.status(400).json({
+            error: "Invalid watering method"
+        });
+    }
+
+    const result = db.prepare(`
+        INSERT INTO watering_events
+        (amount_ml, method)
+        VALUES (?, ?)
+    `).run(
+        amount_ml,
+        method
+    );
+
+    res.json({
+        success: true,
+        id: result.lastInsertRowid
+    });
+});
+
+
+//esp32 gets settings
+app.get("/api/esp32/settings", (req, res) => {
+    const settings = db.prepare(`
+        SELECT
+            watering_interval,
+            moisture_threshold,
+            watering_amount,
+            watering_mode
+        FROM settings
+        WHERE id = 1
+    `).get();
+
+    res.json(settings);
+});
+
+//esp32 report itself
+app.post("/api/esp32/heartbeat", (req, res) => {
+    const {
+        device_id
+    } = req.body;
+
+    if (!device_id) {
+        return res.status(400).json({
+            error: "Missing device ID"
+        });
+    }
+
+    db.prepare(`
+        UPDATE device
+        SET
+            device_id = ?,
+            last_seen = CURRENT_TIMESTAMP
+        WHERE id = 1
+    `).run(device_id);
+
+    res.json({
+        success: true
+    });
+});
+
+
+//start server
+app.listen(PORT, () => {
+    console.log(`FLOWERBASE server running at http://localhost:${PORT}`);
+});
