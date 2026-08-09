c24b5f91d8b573c1101a6517997e5f6043604b12
Sophia <sophia.jingwenshao@gmail.com>
2026-08-09 15:12:46 +0200

    Connnected to moisture sensor
    Got signal. However, unsure how to make changes in the main folder pwpESP to subfolder pwpESP

--- plant_watering_project_ESP/src/main.cpp
+++ plant_watering_project_ESP/src/main.cpp
@@ -1,10 +1,16 @@
 #include <Arduino.h>
+const int moisturePin = 34;
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  Serial.println("Hi world");
+  int value = analogRead(moisturePin);
+
+  Serial.print("Fuktighetsverdi: ");
+  Serial.println(value);
+
   delay(1000);
+             
 }
